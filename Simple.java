a3e8ac59e0ef958cd21fd5915421caf1182e9032
jayprbhakar <98215030+Jayprbhakar@users.noreply.github.com>
2023-08-04 19:59:49 +0530

    Java simple programs

---
+++ Simple.java
@@ -0,0 +1,15 @@
+package Simplex;
+public class Simple {
+
+	public static void main(String[] args) {
+//		ternary operator if-then-else
+		
+		int monday = 2;
+		String tuesday; 
+		
+		tuesday = (monday == 2) ? "3 august" : "2 august";
+		
+		System.out.println(tuesday);		
+	}
+
+}
